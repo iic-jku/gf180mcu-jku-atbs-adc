69bebed847f65f0b79d558ae1b0fc35ca22ae069
Simon Dorrer <simon.dorrer@jku.at>
2025-12-10 22:00:08 +0100

    updated vhdl2verilog.sh

--- macros/tbs_core_board/verilog/src/vhdl2verilog.sh
+++ macros/tbs_core_board/verilog/src/vhdl2verilog.sh
@@ -11,29 +11,26 @@ set -e -x
 
 cd $(dirname "$0")
 
-# Name as input parameter (atbs_core_floating_window, atbs_core_fixed_window)
-name=$1
+ATBS_CORE_SIM=${ATBS_CORE_SIM:-../../vhdl/sim/vhdl}
+ADAPTIVE_THRESHOLD_CONTROL_SIM=${ADAPTIVE_THRESHOLD_CONTROL_SIM:-../../vhdl/adaptive_threshold_control/sim/vhdl}
 
-ATBS_CORE_SIM=${ATBS_CORE_SIM:-../../../vhdl/"$name"/sim/vhdl}
-ADAPTIVE_THRESHOLD_CONTROL_SIM=${ADAPTIVE_THRESHOLD_CONTROL_SIM:-../../../vhdl/"$name"/adaptive_threshold_control/sim/vhdl}
-
-PRIORITY_ENCODER_RTL=${PRIORITY_ENCODER_RTL:-../../../vhdl/"$name"/adaptive_threshold_control/Priority-Encoder/rtl}
-SPIKE2THERMOCODE_RTL=${SPIKE2THERMOCODE_RTL:-../../../vhdl/"$name"/adaptive_threshold_control/Spike-2-Thermocode/rtl}
-SPIKE_BUFFERING_RTL=${SPIKE_BUFFERING_RTL:-../../../vhdl/"$name"/adaptive_threshold_control/Spike-Buffering/rtl}
-WEYLS_DISCREPANCY_RTL=${WEYLS_DISCREPANCY_RTL:-../../../vhdl/"$name"/adaptive_threshold_control/Weyls-Discrepancy/rtl}
-ADAPTIVE_THRESHOLD_CONTROL_RTL=${ADAPTIVE_THRESHOLD_CONTROL_RTL:-../../../vhdl/"$name"/adaptive_threshold_control/rtl}
-ANALOG_TRIGGER_RTL=${ANALOG_TRIGGER_RTL:-../../../vhdl/"$name"/analog_trigger/rtl}
-DAC_CONTROL_RTL=${DAC_CONTROL_RTL:-../../../vhdl/"$name"/dac_control/rtl}
-DEBOUNCER_RTL=${DEBOUNCER_RTL:-../../../vhdl/"$name"/debouncer/rtl}
-MEMORY2UART_RTL=${MEMORY2UART_RTL:-../../../vhdl/"$name"/memory2uart/rtl}
-SC_NOC_GENERATOR_RTL=${SC_NOC_GENERATOR_RTL:-../../../vhdl/"$name"/sc_noc_generator/rtl}
-SPIKE_DETECTOR_RTL=${SPIKE_DETECTOR_RTL:-../../../vhdl/"$name"/spike_detector/rtl}
-SPIKE_ENCODER_RTL=${SPIKE_ENCODER_RTL:-../../../vhdl/"$name"/spike_encoder/rtl}
-SPIKE_MEMORY_RTL=${SPIKE_MEMORY_RTL:-../../../vhdl/"$name"/spike_memory/rtl}
-SYNC_CHAIN_RTL=${SYNC_CHAIN_RTL:-../../../vhdl/"$name"/sync_chain/rtl}
-TIME_MEASUREMENT_RTL=${TIME_MEASUREMENT_RTL:-../../../vhdl/"$name"/time_measurement/rtl}
-UART_RTL=${UART_RTL:-../../../vhdl/"$name"/uart/rtl}
-ATBS_CORE_RTL=${ATBS_CORE_RTL:-../../../vhdl/"$name"/rtl}
+PRIORITY_ENCODER_RTL=${PRIORITY_ENCODER_RTL:-../../vhdl/adaptive_threshold_control/Priority-Encoder/rtl}
+SPIKE2THERMOCODE_RTL=${SPIKE2THERMOCODE_RTL:-../../vhdl/adaptive_threshold_control/Spike-2-Thermocode/rtl}
+SPIKE_BUFFERING_RTL=${SPIKE_BUFFERING_RTL:-../../vhdl/adaptive_threshold_control/Spike-Buffering/rtl}
+WEYLS_DISCREPANCY_RTL=${WEYLS_DISCREPANCY_RTL:-../../vhdl/adaptive_threshold_control/Weyls-Discrepancy/rtl}
+ADAPTIVE_THRESHOLD_CONTROL_RTL=${ADAPTIVE_THRESHOLD_CONTROL_RTL:-../../vhdl/adaptive_threshold_control/rtl}
+ANALOG_TRIGGER_RTL=${ANALOG_TRIGGER_RTL:-../../vhdl/analog_trigger/rtl}
+DAC_CONTROL_RTL=${DAC_CONTROL_RTL:-../../vhdl/dac_control/rtl}
+DEBOUNCER_RTL=${DEBOUNCER_RTL:-../../vhdl/debouncer/rtl}
+MEMORY2UART_RTL=${MEMORY2UART_RTL:-../../vhdl/memory2uart/rtl}
+SC_NOC_GENERATOR_RTL=${SC_NOC_GENERATOR_RTL:-../../vhdl/sc_noc_generator/rtl}
+SPIKE_DETECTOR_RTL=${SPIKE_DETECTOR_RTL:-../../vhdl/spike_detector/rtl}
+SPIKE_ENCODER_RTL=${SPIKE_ENCODER_RTL:-../../vhdl/spike_encoder/rtl}
+SPIKE_MEMORY_RTL=${SPIKE_MEMORY_RTL:-../../vhdl/spike_memory/rtl}
+SYNC_CHAIN_RTL=${SYNC_CHAIN_RTL:-../../vhdl/sync_chain/rtl}
+TIME_MEASUREMENT_RTL=${TIME_MEASUREMENT_RTL:-../../vhdl/time_measurement/rtl}
+UART_RTL=${UART_RTL:-../../vhdl/uart/rtl}
+ATBS_CORE_RTL=${ATBS_CORE_RTL:-../../vhdl/rtl}
 SRC_FOLDER=${SRC_FOLDER:-.}
 
 mkdir -p build
@@ -63,17 +60,17 @@ ghdl -a --std=93c --work=TBS --workdir=build -Pbuild \
   "$UART_RTL"/uart_rx_ea.vhd \
   "$UART_RTL"/uart_tx_ea.vhd \
   "$UART_RTL"/uart_ea.vhd \
-  "$ATBS_CORE_RTL"/"$name"_ea.vhd \
-  "$ATBS_CORE_RTL"/"$name"_board.vhd
+  "$ATBS_CORE_RTL"/tbs_core_ea.vhd \
+  "$ATBS_CORE_RTL"/tbs_core_board.vhd
 
 # Top entity
-ghdl -m --std=93c --work=TBS --workdir=build -Pbuild "$name"_board
+ghdl -m --std=93c --work=TBS --workdir=build -Pbuild tbs_core_board
 
 # Synthesize: generate Verilog output
-ghdl synth --std=93c --work=TBS --workdir=build -Pbuild --out=verilog "$name"_board > "$SRC_FOLDER"/"$name"_board.v
+ghdl synth --std=93c --work=TBS --workdir=build -Pbuild --out=verilog tbs_core_board > "$SRC_FOLDER"/tbs_core_board.v
 
 # Show interface of generated Verilog module
 echo ""
-echo "------ "$name"_board interface ------"
-sed -n "/module "$name"_board/,/);/p" "$SRC_FOLDER"/"$name"_board.v
+echo "------ tbs_core_board interface ------"
+sed -n "/module tbs_core_board/,/);/p" "$SRC_FOLDER"/tbs_core_board.v
 echo ""
