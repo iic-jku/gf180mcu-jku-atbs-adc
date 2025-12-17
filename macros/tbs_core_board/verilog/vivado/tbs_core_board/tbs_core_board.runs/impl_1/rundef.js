b759eb3ddfb3ffe1b8723eeabc4030c68452ecba
Simon Dorrer <simon.dorrer@jku.at>
2025-12-17 17:39:08 +0100

    reran all simulations and synthesized Verilog and VHDL versions with Vivado --> ready for LibreLane & FPGA test

--- macros/tbs_core_board/verilog/vivado/tbs_core_board/tbs_core_board.runs/impl_1/rundef.js
+++ macros/tbs_core_board/verilog/vivado/tbs_core_board/tbs_core_board.runs/impl_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log tbs_core_board.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source tbs_core_board.tcl -notrace" );
 
